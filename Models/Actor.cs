1c5f06e4f49f8d9623ec75bfb7b17e8fecf5109b
user <user@DESKTOP-P6HJ6RV>
2024-03-13 16:12:49 +0200

    Add project files.

---
+++ Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EticketsApp.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Profile Picture URL")]
+        public string ProfilePictureUrl { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+
+        public List<Movie> Movies { get; set; }
+    }
+}
---
+++ Models/Producer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EticketsApp.Models
+{
+    public class Producer
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+
+        public List<Movie> Movies { get; set; }
+    }
+}
